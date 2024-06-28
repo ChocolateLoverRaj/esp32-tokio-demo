6452d9f1637725d62f11170a9e82c7b28b3c1670
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-06-28 11:25:07 -0700

    HTTP and WebSocket

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
+use std::convert::Infallible;
 use esp_idf_svc::hal::prelude::Peripherals;
 use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::timer::EspTaskTimerService;
 use esp_idf_svc::wifi::{AsyncWifi, EspWifi, AuthMethod, ClientConfiguration, Configuration};
 use esp_idf_svc::sys::{esp, EspError};
+use http_body_util::{BodyExt, Empty, Full};
+use http_body_util::combinators::BoxBody;
+use hyper::{Method, Request, Response, StatusCode};
+use hyper::body::Bytes;
+use hyper::service::service_fn;
+use hyper_tungstenite::HyperWebsocket;
+use hyper_tungstenite::tungstenite::Message;
+use hyper_util::rt::TokioIo;
 use log::info;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use futures::sink::SinkExt;
+use futures::stream::StreamExt;
 
 // Edit these or provide your own way of provisioning...
-const WIFI_SSID: &str = "The password is password";
-const WIFI_PASS: &str = "password";
+const WIFI_SSID: &str = "AJR2";
+const WIFI_PASS: &str = "Rnr12345";
 
 // To test, run `cargo run`, then when the server is up, use `nc -v espressif 12345` from
 // a machine on the same Wi-Fi network.
-const TCP_LISTENING_PORT: u16 = 12345;
+const TCP_LISTENING_PORT: u16 = 80;
 
 fn main() -> anyhow::Result<()> {
   // It is necessary to call this function once. Otherwise, some patches to the runtime
@@ -124,34 +135,110 @@ async fn echo_server() -> anyhow::Result<()> {
 
   loop {
     info!("Waiting for new connection on socket: {listener:?}");
-    let (socket, _) = listener.accept().await?;
+    let (stream, _) = listener.accept().await?;
 
-    info!("Spawning handle for: {socket:?}...");
+    let io = TokioIo::new(stream);
+
     tokio::spawn(async move {
       info!("Spawned handler!");
-      let peer = socket.peer_addr();
-      if let Err(e) = serve_client(socket).await {
-        info!("Got error handling {peer:?}: {e:?}");
+      if let Err(err) = hyper::server::conn::http1::Builder::new()
+          .keep_alive(true)
+          // `service_fn` converts our function in a `Service`
+          .serve_connection(io, service_fn(handle_request))
+          .with_upgrades()
+          .await
+      {
+        eprintln!("Error serving connection: {:?}", err);
       }
     });
   }
 }
 
-async fn serve_client(mut stream: TcpStream) -> anyhow::Result<()> {
-  info!("Handling {stream:?}...");
+async fn handle_request(mut req: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, Error> {
+  // We create some utility functions to make Empty and Full bodies
+// fit our broadened Response body type.
+  fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+  }
+  fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+  }
 
-  let mut buf = [0u8; 512];
-  loop {
-    info!("About to read...");
-    let n = stream.read(&mut buf).await?;
-    info!("Read {n} bytes...");
+  // Check if the request is a websocket upgrade request.
+  if hyper_tungstenite::is_upgrade_request(&req) {
+    let (response, websocket) = hyper_tungstenite::upgrade(&mut req, None)?;
 
-    if n == 0 {
-      break;
+    // Spawn a task to handle the websocket connection.
+    tokio::spawn(async move {
+      if let Err(e) = serve_websocket(websocket).await {
+        eprintln!("Error in websocket connection: {e}");
+      }
+    });
+
+    // Return the response so the spawned future can continue.
+    Ok(response.map(|a| a.map_err(|never| match never {}).boxed()))
+  } else {
+    // Handle regular HTTP requests here.
+    match (req.method(), req.uri().path()) {
+      (&Method::GET, uri) => {
+        match uri {
+          "/" => Ok(Response::new(full("This will be where index.html is served"))),
+          _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+          }
+        }
+      },
+      // Return 404 Not Found for other routes.
+      _ => {
+        let mut not_found = Response::new(empty());
+        *not_found.status_mut() = StatusCode::NOT_FOUND;
+        Ok(not_found)
+      }
     }
+  }
+}
 
-    stream.write_all(&buf[0..n]).await?;
-    info!("Wrote {n} bytes back...");
+
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+/// Handle a websocket connection.
+async fn serve_websocket(websocket: HyperWebsocket) -> Result<(), Error> {
+  let mut websocket = websocket.await?;
+  while let Some(message) = websocket.next().await {
+    match message? {
+      Message::Text(msg) => {
+        println!("Received text message: {msg}");
+        websocket.send(Message::text("Thank you, come again.")).await?;
+      },
+      Message::Binary(msg) => {
+        println!("Received binary message: {msg:02X?}");
+        websocket.send(Message::binary(b"Thank you, come again.".to_vec())).await?;
+      },
+      Message::Ping(msg) => {
+        // No need to send a reply: tungstenite takes care of this for you.
+        println!("Received ping message: {msg:02X?}");
+      },
+      Message::Pong(msg) => {
+        println!("Received pong message: {msg:02X?}");
+      }
+      Message::Close(msg) => {
+        // No need to send a reply: tungstenite takes care of this for you.
+        if let Some(msg) = &msg {
+          println!("Received close message with code {} and message: {}", msg.code, msg.reason);
+        } else {
+          println!("Received close message");
+        }
+      },
+      Message::Frame(_msg) => {
+        unreachable!();
+      }
+    }
   }
 
   Ok(())
